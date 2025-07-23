bfaa513099a0c5fba74a28d2e093739f041bcc2a
AsetGithub <160106579+AsetGithub@users.noreply.github.com>
2025-07-23 14:03:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 
-# Ambil token dari environment variable yang akan kita set nanti
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-ALCHEMY_WSS_URL = os.getenv('ALCHEMY_WSS_URL')
+# Pastikan variabel ini yang digunakan, bukan ALCHEMY_WSS_URL
+ALCHEMY_API_KEY = os.getenv('ALCHEMY_API_KEY')
 
-# Cek apakah token ada, jika tidak, program akan berhenti dengan pesan error
 if not TELEGRAM_TOKEN:
     raise ValueError("Token Telegram tidak ditemukan! Mohon set environment variable TELEGRAM_TOKEN.")
 
-if not ALCHEMY_WSS_URL:
-    raise ValueError("URL WebSocket Alchemy tidak ditemukan! Mohon set environment variable ALCHEMY_WSS_URL.")
+if not ALCHEMY_API_KEY:
+    raise ValueError("Kunci API Alchemy tidak ditemukan! Mohon set environment variable ALCHEMY_API_KEY.")
